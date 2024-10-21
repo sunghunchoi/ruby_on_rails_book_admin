e9334fe8472b3082cddf3ad3e0d227660567abc0
sunghun choi <csh6222sp@gmail.com>
2024-10-21 21:50:57 +0900

    [add]: 多対多関係のリレーション作成、モデルバリデーション追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,26 @@ class Book < ApplicationRecord
   scope :written_about, ->(theme) { where("name like ?", "%#{theme}%") }
 
   belongs_to :publisher
+  has_many :book_authors
+  has_many :authors, through: :book_authors
+
+  validates :name, presence: true
+  validates :name, length: { maximum: 25 }
+  validates :price, numericality: { greater_then_or_equal_to: 0}
+
+  before_validation do
+    self.name = self.name.gsub(/Cat/) do |matched|
+      "lovely #{matched}"
+    end
+  end
+
+  after_destroy do
+    Rails.logger.info "Book is deleted: #{self.attributes}"
+  end
+
+  # validates do |book|
+  #   if book.name.include?("exercise")
+  #     book.errors[:name] << "I don't like exercise"
+  #   end
+  # end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_18_130030) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_21_060508) do
   create_table "authors", force: :cascade do |t|
     t.string "name"
     t.string "penname"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "book_authors", force: :cascade do |t|
+    t.integer "book_id", null: false
+    t.integer "author_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_book_authors_on_author_id"
+    t.index ["book_id"], name: "index_book_authors_on_book_id"
   end
 
   create_table "books", force: :cascade do |t|
@@ -35,5 +44,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_18_130030) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "book_authors", "authors"
+  add_foreign_key "book_authors", "books"
   add_foreign_key "books", "publishers"
 end

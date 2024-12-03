8c0916b92e08869112d90825dc23fd7582985347
sunghun choi <csh6222sp@gmail.com>
2024-12-03 09:41:47 +0900

    add callback def

--- app/models/book.rb
+++ app/models/book.rb
@@ -16,8 +16,13 @@ class Book < ApplicationRecord
     end
   end
 
-  after_destroy do
-    Rails.logger.info "Book is deleted: #{self.attributes}"
+  after_destroy :if => :high_price? do
+    Rails.logger.warn "Book with high price is deleted: #{self.attributes}"
+    Rails.logger.warn "Please check!!"
+  end
+
+  def high_price?
+    price >= 5000
   end
 
   # validates do |book|
